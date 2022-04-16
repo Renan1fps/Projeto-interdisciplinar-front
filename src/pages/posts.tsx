7f7a47155be58eb25e37bdaad0de0ff31ade2e5a
MbRenan <renan.sergipe68@gmail.com>
2022-04-16 18:25:38 -0300

    feat: init page posts

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,5 +1,9 @@
+import { Container } from "../components/container"
+
 export default function Posts (){
   return (
-    <h1>Posts</h1>
+    <Container>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium distinctio aliquid officia expedita repudiandae ab recusandae laborum veniam, repellat sint, aut consequuntur. Soluta voluptatum molestiae fugiat corrupti pariatur, voluptate incidunt.</p>
+    </Container>
   )
 }
