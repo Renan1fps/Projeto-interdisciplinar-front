cc09f317495679711fbbfc30edfe3658994495b7
MbRenan <renan.sergipe68@gmail.com>
2022-04-16 18:24:08 -0300

    feat: create container component

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,5 +1,10 @@
-export function Container({children}){
-  <div>
-    {children}
-  </div>
+import style from './style.module.scss'
+
+export function Container({ children }) {
+  return (
+    <div className={style.container}>
+      <h1 style={{marginBottom: '2rem'}}>Postagens</h1>
+      {children}
+    </div>
+  )
 }
