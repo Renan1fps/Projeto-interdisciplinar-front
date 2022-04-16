8e7b4eb234af0269972586d229cb28ba912bbc4d
MbRenan <renan.sergipe68@gmail.com>
2022-04-16 18:24:56 -0300

    refactor:  change SigninButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,34 @@
-import { SigninButton } from '../SigninButton'
+import { useEffect, useState } from 'react'
+import { GenericModal } from '../modal'
 import style from './style.module.scss'
-export function Header(){
+import { signOut } from "next-auth/react"
+
+
+export function Header({ isLogged }) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  function handleCloseModal() {
+    setIsModalOpen(false)
+  }
+
+
   return (
     <header className={style.headerContainer}>
       <div className={style.headerContent}>
-        <img src="/images/logoBranco.png" alt="v2 logo" width='250px'/>
+        <img src="/images/logoBranco.png" alt="v2 logo" width='250px' />
         <nav>
           <a className={style.active}>Home</a>
           <a href='/posts'>Posts</a>
         </nav>
-        <SigninButton/>
+        {isLogged === true ?
+          <button className={style.signinButton} onClick={() => {signOut(); localStorage.removeItem('logged')}} >
+            <span style={{color: '#61dafb'}}>Renan</span>
+          </button> :
+          <button className={style.signinButton} onClick={() => setIsModalOpen(true)}>
+            Entrar
+            <GenericModal isOpen={isModalOpen} onRequestClose={() => handleCloseModal()} />
+          </button>
+        }
       </div>
     </header>
   )
